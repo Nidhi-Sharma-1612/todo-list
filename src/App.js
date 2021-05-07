63d438412fb2643e057fb19489f8866e2bbdfc6f
Nidhi-Sharma-1612 <80857922+Nidhi-Sharma-1612@users.noreply.github.com>
2021-05-07 12:24:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import "./App.css";
+import Header from "./myComponents/Header";
+import { Footer } from "./myComponents/Footer";
+import { Todos } from "./myComponents/Todos";
+import { AddTodo } from "./myComponents/AddTodo";
+import { About } from "./myComponents/About";
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+function App() {
+  let initTodo;
+  if (localStorage.getItem("todos") === null) {
+    initTodo = [];
+  } else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
+  const onDelete = (todo) => {
+    console.log("I am onDelete of todo", todo);
+    setTodos(
+      todos.filter((e) => {
+        return e !== todo;
+      })
+    );
+    localStorage.getItem("todos");
+  };
+
+  const addTodo = (title, desc) => {
+    console.log("I am adding addTodo", title, desc);
+    let sno;
+    if (todos.length === 0) {
+      sno = 0;
+    } else {
+      sno = todos[todos.length - 1].sno + 1;
+    }
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc,
+    };
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);
+    localStorage.getItem("todos");
+  };
+
+  const [todos, setTodos] = useState(initTodo);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  return (
+    <>
+    <Router>
+      <Header title="My Todos List" searchBar={false} />
+      <Switch>
+          <Route exact path="/" render = {()=>{
+            return(
+            <>
+            <AddTodo addTodo={addTodo} />
+            <Todos todos={todos} onDelete={onDelete} />
+            </>)
+          }}>
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route> 
+      </Switch>
+      <Footer />
+    </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/myComponents/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <strong>This is an about page. </strong>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                Unde dolorum expedita totam nam amet beatae perferendis, assumenda veritatis explicabo, cumque facere incidunt? 
+                Numquam odit expedita delectus, porro labore doloremque totam!
+            </p>
+        </div>
+    )
+}
+
